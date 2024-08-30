166fa835d0eb41845771e870a7ae62c99692240e
Nishant. <98388538+NishantKB@users.noreply.github.com>
2024-08-30 22:17:34 +0545

    removed the img tag showing image icon until image is loaded

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,13 @@
 function generateQRCode() {
     const text = document.getElementById('text-input').value;
-    if (!text) {
-        alert('Please enter text to generate a QR code.');
-        return;
-    }
     QRCode.toDataURL(text, { errorCorrectionLevel: 'H' }, function (err, url) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        document.getElementById('qrcode').src = url;
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const qrcodeImg = document.getElementById('qrcode');
+      qrcodeImg.src = url;
+      qrcodeImg.style.display = 'block'; 
     });
-}
+  }
+  
